466be35e07a3f271899f21b2f90577bd081ea5fa
yasiruranathunga <yasirusr99@gmail.com>
2022-09-08 20:19:04 +0530

    8

--- spm-front/src/Navigation/Navigation.jsx
+++ spm-front/src/Navigation/Navigation.jsx
@@ -3,11 +3,12 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
+import AdminDashboard from '../Pages/UserManagement/AdminDashboard';
 
 function Navigation() {
     return (
         <Routes>
-            <Route path='/' element={<div>Navigation</div>} />
+            <Route path='/' element={<AdminDashboard />} />
             <Route path='/test' element={<div>test</div>} />
         </Routes>
     )
